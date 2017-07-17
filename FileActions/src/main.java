929909ac508a71279c5fd253ec52f5855aaced42
huynguyencs <huy8@pdx.edu>
2017-07-16 23:15:52 -0700

    Log in to FTP server

--- FileActions/src/main.java
+++ FileActions/src/main.java
@@ -1,8 +1,45 @@
 import java.io.*;
+import org.apache.commons.net.ftp.FTPClient;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Scanner;
 
 class FileActions {
     public static void main(String[] args){
-        
-        System.exit(1);
+        Scanner scan = new Scanner(System.in);
+        FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.connect("127.0.0.1"); //LOCAL HOST
+            System.out.println("Enter user name: ");
+            String username = scan.nextLine();
+            System.out.println("Enter password: ");
+            String password = scan.nextLine();
+            boolean login = ftpClient.login(username, password);
+            if (login) {
+                System.out.println("Connection established...");
+                System.out.println("Status: "+ftpClient.getStatus());
+
+
+                // logout the user, returned true if logout successfully
+                boolean logout = ftpClient.logout();
+                if (logout) {
+                    System.out.println("Connection close...");
+                }
+            } else {
+                System.out.println("Connection fail...");
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ftpClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    System.exit(1);
     }
 }
