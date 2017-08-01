99a8dc0ad49bdc48eb20636b8df7fc6cb610e6e6
chsherpa <chsherpa@pdx.edu>
2017-08-01 11:21:32 -0700

    Moved most methods to getFile.java

--- main/src/group7/ftp/Main.java
+++ main/src/group7/ftp/Main.java
@@ -1,125 +1,39 @@
 package group7.ftp;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
 public class Main {
+  String defaultUser = new String();
+  String defaultPwd = new String();
+  int defaultPort = 21;
 
-    /**
-     * Method takes in four args for the source and destination of the file name args;
-     * Looks it up on the client side and returns true or false for success
-     * If found, instant write to destination path
-     * All input param are assumed to be valid ( i.e. sourcePath exists, destPath exists,
-     * FTP Client is logged on with proper credentials )
-     *
-     * Source: http://www.codejava.net/java-se/networking/ftp/java-ftp-file-download-tutorial-and-example
-     *
-     * @param sourcePath    Path where the file you're looking for comes from
-     * @param destPath      Path where you want the file to go on your local machine
-     * @param File          Name of the File you're looking for
-     * @return              Boolean value; true equals success
-     */
-    public static boolean getFile( String sourcePath, String destPath,
-                                   String File, FTPClient ftpClient){
-        String remoteFile1 = new String( sourcePath+"/"+File);
-        String downloadPath = new String( destPath+"/"+File);
-        File downloadFile1 = new File(downloadPath);
-        try{
-            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
-            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
-            outputStream1.close();
-            if (success) {
-                System.out.println("File has been downloaded successfully.");
-                return true;
-            }
-        }
-        catch(IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-        return false;
+  public static void main( String[] args ){
+    /*
+    String sourcePath = new String();
+    String destPath = new String();
+    String file = new String();
+
+    FTPClient ftpClient = new FTPClient();
+
+    try{
+        isDirectoryOnLocal(destPath);
+    }
+    catch(IllegalArgumentException ex){
+        System.out.println("\n"+destPath+" is not a directory");
     }
 
-    /**
-     * Multiple version of the getFile method
-     * @param sourcePath    Path where the file you're looking for comes from
-     * @param destPath      Path where you want the file to go on your local machine
-     * @param Files         Name of the File you're looking for
-     * @param ftpClient     Boolean value; true equals success
-     * @return              Boolean value; true equals success
-     */
-    public static boolean getMultipleFiles( String sourcePath, String destPath,
-                                            String[] Files, FTPClient ftpClient ){
-        boolean success = false;
-        for( int i = 0; i < Files.length; i++){
-            success = getFile(sourcePath, destPath, Files[i], ftpClient);
-        }
-        return success;
-    }
+    //Temporary Testing
+    if( getFile(sourcePath, destPath, file, ftpClient) )
+        System.out.println( "GetFile Works");
 
-    /**
-     * A method to check whether the passed in path is a directory on the local
-     * machine. Returns false if it is not
-     *
-     * @param Path          A valid path to a directory
-     * @return  Boolean     Value True is success
-     */
-    public static boolean isDirectoryOnLocal( String Path ){
-        File dir = new File(Path);
-        return dir.isDirectory();
-    }
+    //Temporary Testing
+    String[] files = new String[5];
+    if( getMultipleFiles(sourcePath, destPath, files, ftpClient) )
+        System.out.println("GetMultiple Files Works!");
 
-    /**
-     * Call this method to get the list of the files on the current
-     * directory
-     *
-     * @return  Always returns true
-     */
-    public static boolean listFilesOnCurrentDirectory(){
-        File currDir = new File(".");
+    listFilesOnCurrentDirectory();
+    */
 
-        if( isDirectoryOnLocal( currDir.toString() ) == false ) return false;
-
-        File[] filesList = currDir.listFiles();
-        for(int i = 0; i < filesList.length; i++ ){
-            System.out.println( filesList[i].getName() );
-        }
-        return true;
-    }
-
-    public static void main( String[] args ){
-        System.out.printf("Hello, world!");
-        String sourcePath = new String();
-        String destPath = new String();
-        String file = new String();
-        FTPClient ftpClient = new FTPClient();
-
-        try{
-            isDirectoryOnLocal(destPath);
-        }
-        catch(IllegalArgumentException ex){
-            System.out.println("\n"+destPath+" is not a directory");
-        }
-
-        //Temporary Testing
-        if( getFile(sourcePath, destPath, file, ftpClient) )
-            System.out.println( "GetFile Works");
-
-        //Temporary Testing
-        String[] files = new String[5];
-        if( getMultipleFiles(sourcePath, destPath, files, ftpClient) )
-            System.out.println("GetMultiple Files Works!");
-
-        listFilesOnCurrentDirectory();
-
-        System.exit(1);
-    }
+    System.exit(1);
+  }
 }
---
+++ main/src/group7/ftp/getFile.java
@@ -0,0 +1,152 @@
+package group7.ftp;
+
+import org.apache.commons.net.ftp.FTPClient;
+import java.io.*;
+import java.net.SocketException;
+
+/**
+ * Created by chsherpa on 7/22/17.
+ */
+public class getFile
+{
+  private boolean Login(String username, String password)
+  {
+    FTPClient ftp = new FTPClient();
+    try
+    {
+      ftp.connect("ftp.swfwmd.state.fl.us",21);
+      boolean login = ftp.login(username, password);
+      if (login)
+      {
+        System.out.println("Connection established...");
+        System.out.println("Status: " + ftp.getStatus());
+
+        // logout the user, returned true if logout successfully
+        boolean logout = ftp.logout();
+        if (logout)
+        {
+          return true;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
+    catch (SocketException e)
+    {
+      e.printStackTrace();
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      try
+      {
+        ftp.disconnect();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
+    return false;
+  }
+
+  public getFile()
+  { }
+
+  public boolean SingleFile( String sourcePath, String destPath, String fileName, FTPClient ftp )
+  {
+    return getFile(sourcePath, destPath, fileName, ftp );
+  }
+
+  public boolean multipleFiles( String sourcePath, String destPath, String[] files, FTPClient ftp )
+  {
+    return getMultipleFiles( sourcePath, destPath, files, ftp );
+  }
+
+  /**
+   * A method to check whether the passed in path is a directory on the local
+   * machine. Returns false if it is not
+   *
+   * @param Path          A valid path to a directory
+   * @return  Boolean     Value True is success
+   */
+  public boolean isDirectoryOnLocal( String Path ){
+      File dir = new File(Path);
+      return dir.isDirectory();
+  }
+
+  /**
+   * Call this method to get the list of the files on the current
+   * directory
+   *
+   * @return  boolean value of True/False; True being success value
+   */
+  public boolean listFilesOnCurrentDirectory(){
+      File currDir = new File(".");
+
+      if( isDirectoryOnLocal( currDir.toString() ) == false ) return false;
+
+      File[] filesList = currDir.listFiles();
+      for(int i = 0; i < filesList.length; i++ ){
+          System.out.println( filesList[i].getName() );
+      }
+      return true;
+  }
+
+  /**
+  * Method takes in four args for the source and destination of the file name args;
+  * Looks it up on the client side and returns true or false for success
+  * If found, instant write to destination path
+  * All input param are assumed to be valid ( i.e. sourcePath exists, destPath exists,
+  * FTP Client is logged on with proper credentials )
+  *
+  * Source: http://www.codejava.net/java-se/networking/ftp/java-ftp-file-download-tutorial-and-example
+  *
+  * @param sourcePath    Path where the file you're looking for comes from
+  * @param destPath      Path where you want the file to go on your local machine
+  * @param fileName          Name of the File you're looking for
+  * @return              Boolean value; true equals success
+  */
+  private boolean getFile( String sourcePath, String destPath, String fileName, FTPClient client)
+  {
+    String remoteFile1 = new String( sourcePath+"/"+fileName);
+    String downloadPath = new String( destPath+"/"+fileName);
+    File downloadFile1 = new File(downloadPath);
+    try{
+        OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+        boolean success = client.retrieveFile(remoteFile1, outputStream1);
+        outputStream1.close();
+        if (success) {
+          System.out.println("File has been downloaded successfully.");
+          return true;
+        }
+    }
+    catch(IOException ex) {
+      System.out.println("Error: " + ex.getMessage());
+      System.out.println();
+    }
+  return false;
+  }
+
+  /**
+   * Multiple version of the getFile method
+   * @param sourcePath    Path where the file you're looking for comes from
+   * @param destPath      Path where you want the file to go on your local machine
+   * @param files         Name of the File you're looking for
+   * @param ftp     Boolean value; true equals success
+   * @return              Boolean value; true equals success
+   */
+  private boolean getMultipleFiles( String sourcePath, String destPath, String[] files, FTPClient ftp )
+  {
+      boolean success = false;
+      for( int i = 0; i < files.length; i++){
+          success = getFile(sourcePath, destPath, files[i], ftp);
+      }
+      return success;
+  }
+}
